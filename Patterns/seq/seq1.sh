01b03eda1151c040d2d2ccb4549135b3e992818a
Anushka <anushka25999@gmail.com>
2020-08-29 21:57:47 +0530

    Updated: Patterns

--- Patterns/seq/seq1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-
-#Use Random Function ((RANDOM)) to get Single Digit
-echo $(( RANDOM%10 ));
-
--- Patterns/seq/seq2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -x
-
-#Use Random to get Dice Number between 1 to 6
-echo "Dice number: $((( RANDOM%6 ) +1 ))";

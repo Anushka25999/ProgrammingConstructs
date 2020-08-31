d1f5f629a17f40512ef3507a8289fd3b5886a0e4
Anushka <anushka25999@gmail.com>
2020-08-31 20:08:11 +0530

    Updated: flipCoinCombination.sh with UC4

--- programmingConstructs/flipCoinCombination.sh
+++ programmingConstructs/flipCoinCombination.sh
@@ -16,11 +16,22 @@ count_TT=0
 count_HT=0
 count_TH=0
 
+#initialize for triplet
+count_HHH=0
+count_HHT=0
+count_HTH=0
+count_HTT=0
+count_THH=0
+count_THT=0
+count_TTH=0
+count_TTT=0
+
 for((count=0;$count < $max_iterations;count++));
 do
 	#get random number between 0,1 and check if its 1
 	flip1=$(($RANDOM%2))
 	flip2=$(($RANDOM%2))
+	flip3=$(($RANDOM%2))
 
 	if(($flip1 == 1 ))
 	then
@@ -39,9 +50,24 @@ do
 		then
 			#if its 1 then increase count of HH
 			count_HH=$(($count_HH + 1))
+
+			if(($flip3==head_flip))
+			then
+				count_HHH=$(($count_HHH + 1))
+			else
+				count_HHT=$(($count_HHT + 1))
+			fi
 		else
 			#else increase count of TT
 			count_TT=$(($count_TT + 1))
+
+			#check if flip3 is head
+			if(($flip3==head_flip))
+			then
+				count_TTH=$(($count_TTH + 1))
+			else
+				count_TTT=$(($count_TTT + 1))
+			fi
 		fi
 	#if first and second flips are different
 	else
@@ -50,9 +76,25 @@ do
 		then
 			#if it's 1 increase HT count by 1
 			count_HT=$(($count_HT + 1))
+
+			#check if third is head
+			if(($flip3==head_flip))
+			then
+				count_HTH=$(($count_HTH + 1))
+			else
+				count_HTT=$(($count_HTT + 1))
+			fi
 		else
 			#else increase count of TH
 			count_TH=$(($count_TH + 1))
+
+			#check if third is head
+			if(($flip3==head_flip))
+			then
+				count_THH=$(($count_THH + 1))
+			else
+				count_THT=$(($count_THT + 1))
+			fi
 		fi
 	fi
 done
@@ -72,4 +114,24 @@ echo "HH percentage : ${flip["HH percentage "]}"
 echo "TT percentage : ${flip["TT percentage "]}"
 echo "HT percentage : ${flip["HT percentage "]}"
 echo "TH percentage : ${flip["TH percentage "]}"
+
+#create dictionary storing percentage
+declare -A flip_triplet
+flip_triplet["HHH percentage "]=$(awk '{print ($1*100/($2))}' <<< "${count_HHH} ${max_iterations}")
+flip_triplet["HHT percentage "]=$(awk '{print ($1*100/($2))}' <<< "${count_HHT} ${max_iterations}")
+flip_triplet["HTH percentage "]=$(awk '{print ($1*100/($2))}' <<< "${count_HTH} ${max_iterations}")
+flip_triplet["HTT percentage "]=$(awk '{print ($1*100/($2))}' <<< "${count_HTT} ${max_iterations}")
+flip_triplet["THH percentage "]=$(awk '{print ($1*100/($2))}' <<< "${count_THH} ${max_iterations}")
+flip_triplet["THT percentage "]=$(awk '{print ($1*100/($2))}' <<< "${count_THT} ${max_iterations}")
+flip_triplet["TTH percentage "]=$(awk '{print ($1*100/($2))}' <<< "${count_TTH} ${max_iterations}")
+flip_triplet["TTT percentage "]=$(awk '{print ($1*100/($2))}' <<< "${count_TTT} ${max_iterations}")
+
+echo "HHH percentage : ${flip_triplet["HHH percentage "]}"
+echo "HHT percentage : ${flip_triplet["HHT percentage "]}"
+echo "HTH percentage : ${flip_triplet["HTH percentage "]}"
+echo "HTT percentage : ${flip_triplet["HTT percentage "]}"
+echo "THH percentage : ${flip_triplet["THH percentage "]}"
+echo "THT percentage : ${flip_triplet["THT percentage "]}"
+echo "TTH percentage : ${flip_triplet["TTH percentage "]}"
+echo "TTT percentage : ${flip_triplet["TTT percentage "]}"
 

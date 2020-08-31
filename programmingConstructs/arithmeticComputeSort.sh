1d6f637839da3680bf4858345a1a750899934fc8
Anushka <anushka25999@gmail.com>
2020-08-31 17:22:58 +0530

    Updated: arithmeticComputeSort.sh with UC7

--- programmingConstructs/arithmeticComputeSort.sh
+++ programmingConstructs/arithmeticComputeSort.sh
@@ -11,8 +11,26 @@ compute[2]=$((a*b+c))
 compute[3]=$((c+a/b))
 compute[4]=$((a%b+c))
 
+count=0
 #read value from dictionary into array
 for x in ${compute[@]}
 do
 	arr[((count++))]=$x
 done
+
+for ((i = 0; i<$count; i++))
+do
+    for((j = 0; j<$count-i-1; j++))
+    do
+        if (( ${arr[j]} < ${arr[$((j+1))]} ))
+        then
+            # swap
+            temp=${arr[j]}
+            arr[$j]=${arr[$((j+1))]}
+            arr[$((j+1))]=$temp
+        fi
+    done
+done
+
+echo "Results in Descending :"
+echo ${arr[@]}

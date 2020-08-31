d2205a564ac4e5a24e388d2f2645cf18cd60c330
Anushka <anushka25999@gmail.com>
2020-08-31 13:52:39 +0530

    Updated: regexPattern.sh with UC1

--- Patterns/regexPattern.sh
+++ Patterns/regexPattern.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
 
-echo "Welcome to User Registration Pattern"
+#turn on external globbing
+shopt -s extglob
+#User registration
+read -p "Enter your First Name " fname
+
+pat="^[[:upper:]]{1}[[:lower:]]{2,}$"
+
+if [[ $fname =~ $pat ]]
+then
+	echo "Valid Username"
+else
+	echo "Invalid Username"
+fi

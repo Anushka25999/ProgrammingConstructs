e1db2771dc1a9a9e6586432e147cef8b14a4a2d1
Anushka <anushka25999@gmail.com>
2020-08-31 14:06:38 +0530

    Updated: regexPattern.sh with UC3

--- Patterns/regexPattern.sh
+++ Patterns/regexPattern.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 
-#turn on external globbing
+# turn on external globbing
 shopt -s extglob
-#User registration
+# User registration
 
-#First Name
+# First Name
 read -p "Enter your First Name " fname
 
 pat="^[[:upper:]]{1}[[:lower:]]{2,}$"
@@ -16,7 +16,7 @@ else
 	echo "Invalid Username"
 fi
 
-#Last Name
+# Last Name
 read -p "Enter your Last Name " lname
 
 pat1="^[[:upper:]]{1}[[:lower:]]{2,}$"
@@ -26,4 +26,16 @@ then
 	echo "Valid Last Name"
 else
 	echo "Invalid Last Name"
+fi
+
+# Email
+read -p "Enter your Email " email
+
+pat2="^[0-9[:lower:][:upper:]]+([._+-][0-9[:lower:][:upper:]]+)*@[0-9[:lower:][:upper:]]+.[[:lower:][:upper:]]{2,4}([.][[:lower:][:upper:]]{2,4})?$"
+
+if [[ $email =~ $pat2 ]]
+then
+	echo "Valid Email Address"
+else
+	echo "Invalid Email Address"
 fi

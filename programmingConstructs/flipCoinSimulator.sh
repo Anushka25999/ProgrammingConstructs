be583235dc66fdebd9abba0406aee80eaef42921
Anushka <anushka25999@gmail.com>
2020-08-31 18:06:54 +0530

    Updated: flipCoinSimulator.sh with UC2

--- programmingConstructs/flipCoinSimulator.sh
+++ programmingConstructs/flipCoinSimulator.sh
@@ -2,9 +2,18 @@
 
 echo "Welcome to flip coin simulator"
 
-if(($(($RANDOM%2)) == 1 ))
-then
-	echo "Heads is winner"
-else
-	echo "Tails is winner"
-fi
+count_heads=0
+count_tails=0
+
+while true
+do
+	if(($(($RANDOM%2)) == 1 ))
+	then
+		count_heads=$(($count_heads + 1))
+	else
+		count_tails=$(($count_tails + 1))
+	fi
+done
+
+echo "Heads won $count_heads times"
+echo "Tails won $count_tails times"

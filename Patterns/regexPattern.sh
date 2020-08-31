7dd7b06283fe60a6b0cec296f65cc3e877d22d09
Anushka <anushka25999@gmail.com>
2020-08-31 14:00:09 +0530

    Updated: regexPattern.sh with UC2

--- Patterns/regexPattern.sh
+++ Patterns/regexPattern.sh
@@ -3,6 +3,8 @@
 #turn on external globbing
 shopt -s extglob
 #User registration
+
+#First Name
 read -p "Enter your First Name " fname
 
 pat="^[[:upper:]]{1}[[:lower:]]{2,}$"
@@ -12,4 +14,16 @@ then
 	echo "Valid Username"
 else
 	echo "Invalid Username"
+fi
+
+#Last Name
+read -p "Enter your Last Name " lname
+
+pat1="^[[:upper:]]{1}[[:lower:]]{2,}$"
+
+if [[ $lname =~ $pat1 ]]
+then
+	echo "Valid Last Name"
+else
+	echo "Invalid Last Name"
 fi
